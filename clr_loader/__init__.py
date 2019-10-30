bcaeee97020f9615ad81415f9b078fd86d30dd45
Benedikt Reinartz <filmor@gmail.com>
2019-10-30 19:44:49 +0100

    Implement simple main interface

---
+++ clr_loader/__init__.py
@@ -0,0 +1,69 @@
+from os.path import basename
+from .ffi import ffi
+
+
+__all__ = ["Runtime"]
+
+
+class ClrFunction:
+    def __init__(self, runtime, assembly, typename, func_name):
+        self._assembly = assembly
+        self._class = typename
+        self._name = func_name
+
+        self._callable = runtime.get_callable(assembly, typename, func_name)
+
+    def __call__(self, buffer):
+        buf_arr = ffi.from_buffer("char[]", buffer)
+        return self._callable(ffi.cast("void*", buf_arr), len(buf_arr))
+
+    def __repr__(self):
+        return f"<ClrFunction {self._class}.{self._name} in {basename(self._assembly)}>"
+
+
+class Assembly:
+    def __init__(self, runtime, path):
+        self._runtime = runtime
+        self._path = path
+
+    def get_function(self, name, func=None):
+        if func is None:
+            name, func = name.rsplit(".", 1)
+
+        return ClrFunction(self._runtime, self._path, name, func)
+
+    def __getitem__(self, name):
+        return self.get_function(name)
+    def __repr__(self):
+        return f"<Assembly {self._path} in {self._runtime}>"
+
+
+class Runtime:
+    def __init__(self, impl):
+        self._impl = impl
+
+    @classmethod
+    def get_mono(cls, domain=None):
+        from .mono import Mono
+        impl = Mono(domain=domain)
+        return cls(impl)
+
+    @classmethod
+    def get_coreclr(cls, runtime_config, dotnet_root=None):
+        from .hostfxr import HostFxr
+
+        impl = HostFxr(runtime_config=runtime_config, dotnet_root=dotnet_root)
+        return cls(impl)
+
+    @classmethod
+    def get_framework(cls, name=None, config_file=None):
+        from .framework import Framework
+
+        impl = Framework(name=name, config_file=config_file)
+        return cls(impl)
+
+    def get_assembly(self, path):
+        return Assembly(self._impl, path)
+
+    def __getitem__(self, path):
+        return self.get_assembly(path)
--- example/Class1.cs
+++ example/Class1.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using System.Text;
+using System.Runtime.InteropServices;
+using System;
 
 namespace example
 {
     public class Class1
     {
     	public static int Test(IntPtr arg, int size) {
-            Console.WriteLine($"Size {size}");
-            return 0;
+            var buf = new byte[size];
+            Marshal.Copy(arg, buf);
+            var bufAsString = Encoding.UTF8.GetString(buf);
+            var result = bufAsString.Length;
+            Console.WriteLine($"Called {nameof(Test)} in {nameof(Class1)} with {bufAsString}, returning {result}");
+            return result;
         }
     }
 }

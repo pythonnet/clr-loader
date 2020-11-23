6df176e8a5689f2e1facd7ae32989948c45fcdad
Benedikt Reinartz <filmor@gmail.com>
2020-11-23 12:54:47 +0100

    Build as netstandard for all but .NET Core

--- tests/test_common.py
+++ tests/test_common.py
@@ -5,40 +5,47 @@ import sys
 
 
 @pytest.fixture(scope="session")
-def example_dll(tmpdir_factory):
-    out = str(tmpdir_factory.mktemp("example"))
+def example_netstandard(tmpdir_factory):
+    return build_example(tmpdir_factory, "netstandard20")
+
+@pytest.fixture(scope="session")
+def example_netcore(tmpdir_factory):
+    return build_example(tmpdir_factory, "netcoreapp31")
+
+def build_example(tmpdir_factory, framework):
+    out = str(tmpdir_factory.mktemp(f"example-{framework}"))
     proj_path = os.path.join(os.path.dirname(__file__), "../example")
 
-    check_call(["dotnet", "build", proj_path, "-o", out, "-f", "netcoreapp31"])
+    check_call(["dotnet", "build", proj_path, "-o", out, "-f", framework])
 
     return out
 
 
 @pytest.mark.xfail
-def test_mono(example_dll):
+def test_mono(example_netstandard):
     from clr_loader import get_mono
 
     mono = get_mono()
-    asm = mono.get_assembly(os.path.join(example_dll, "example.dll"))
+    asm = mono.get_assembly(os.path.join(example_netstandard, "example.dll"))
 
     run_tests(asm)
 
 
-def test_coreclr(example_dll):
+def test_coreclr(example_netcore):
     from clr_loader import get_coreclr
 
-    coreclr = get_coreclr(os.path.join(example_dll, "example.runtimeconfig.json"))
-    asm = coreclr.get_assembly(os.path.join(example_dll, "example.dll"))
+    coreclr = get_coreclr(os.path.join(example_netcore, "example.runtimeconfig.json"))
+    asm = coreclr.get_assembly(os.path.join(example_netcore, "example.dll"))
 
     run_tests(asm)
 
 
 @pytest.mark.skipif(sys.platform != 'win32', reason=".NET Framework only exists on Windows")
-def test_netfx(example_dll):
+def test_netfx(example_netstandard):
     from clr_loader import get_netfx
 
     netfx = get_netfx()
-    asm = netfx.get_assembly(os.path.join(example_dll, "example.dll"))
+    asm = netfx.get_assembly(os.path.join(example_netstandard, "example.dll"))
 
     run_tests(asm)
 

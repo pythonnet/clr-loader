c80ac6a7428a9b28c9d9b7035cb76b9cd3268200
Colton Sellers <Colton.R.Sellers@gmail.com>
2021-03-10 12:48:55 -0800

    byte literall string for config location

--- clr_loader/mono.py
+++ clr_loader/mono.py
@@ -94,7 +94,7 @@ def initialize(config_file: str, libmono: str) -> None:
 
         _ROOT_DOMAIN = _MONO.mono_jit_init(b"clr_loader")
         _MONO.mono_config_parse(config_bytes)
-        _MONO.mono_domain_set_config(_ROOT_DOMAIN, ".", config_file.encode("utf8"))
+        _MONO.mono_domain_set_config(_ROOT_DOMAIN, b".", config_file.encode("utf8"))
         _check_result(_ROOT_DOMAIN, "Failed to initialize Mono")
         atexit.register(_release)
 

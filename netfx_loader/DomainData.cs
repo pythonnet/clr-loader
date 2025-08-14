5dadf4cedbe8e68075d50e626f26a8fc556591af
Mohamed Koubaa <mohamed.koubaa@ansys.com>
2025-08-14 16:44:40 -0500

    Support pythonnet for AppDomain (#78)
    * load the assembly and get function address inside the domain
    
    When using domain.Load for an assembly, the assembly resolution rules are awkward
    Even if the full path is given to the AssemblyName, when the domain tries to load
    the assembly, it does not use that context and tries to resolve the assembly using
    normal domain resolution rules, which would require an assembly resolver to be
    installed. However, the assembly resolver that is actually used at runtime is the
    one installed to the main appdomain.
    
    This prevents a library like Python.Runtime.dll (used by pythonnet) which
    is not installed to the application base directory to be loaded by clr_loader.
    
    To fix this issue, the assembly resolver of the main appdomain is lazily
    extending to include paths needed for libraries passed into GetFunction,
    and GetFunction internally uses AppDomain.DoCallBack() to marshal the
    function pointer inside the target app domain, using global domain data
    to access the function pointer and return it to the user of clr_loader.
    
    * Add comment
    
    * PR review feedback
    
    ---------
    
    Co-authored-by: Mohamed Koubaa <koubaa@github.com>

--- netfx_loader/DomainData.cs
+++ netfx_loader/DomainData.cs
@@ -1,54 +1,115 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace ClrLoader
 {
     using static ClrLoader;
 
-    class DomainData : IDisposable
+    public static class DomainSetup
     {
         public delegate int EntryPoint(IntPtr buffer, int size);
 
+        public static void StoreFunctorFromDomainData()
+        {
+            var domain = AppDomain.CurrentDomain;
+            var assemblyPath = (string)domain.GetData("_assemblyPath");
+            var typeName = (string)domain.GetData("_typeName");
+            var function = (string)domain.GetData("_function");
+            var deleg = GetDelegate(domain, assemblyPath, typeName, function);
+            var functor = Marshal.GetFunctionPointerForDelegate(deleg);
+            domain.SetData("_thisDelegate", deleg);
+            domain.SetData("_thisFunctor", functor);
+        }
+
+        private static Delegate GetDelegate(AppDomain domain, string assemblyPath, string typeName, string function)
+        {
+            var assemblyName = AssemblyName.GetAssemblyName(assemblyPath);
+            var assembly = domain.Load(assemblyName);
+            var type = assembly.GetType(typeName, throwOnError: true);
+            var deleg = Delegate.CreateDelegate(typeof(EntryPoint), type, function);
+            return deleg;
+        }
+    }
+
+    class DomainData : IDisposable
+    {
         bool _disposed = false;
 
         public AppDomain Domain { get; }
-        public Dictionary<(string, string, string), EntryPoint> _delegates;
+        public Dictionary<(string, string, string), IntPtr> _functors;
+        public HashSet<string> _resolvedAssemblies;
 
         public DomainData(AppDomain domain)
         {
             Domain = domain;
-            _delegates = new Dictionary<(string, string, string), EntryPoint>();
+            _functors = new Dictionary<(string, string, string), IntPtr>();
+            _resolvedAssemblies = new HashSet<string>();
         }
 
-        public EntryPoint GetEntryPoint(string assemblyPath, string typeName, string function)
+        private void installResolver(string assemblyPath)
+        {
+            var assemblyName = AssemblyName.GetAssemblyName(assemblyPath).Name;
+            if (_resolvedAssemblies.Contains(assemblyName))
+                return;
+            _resolvedAssemblies.Add(assemblyName);
+
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+                if (args.Name.Contains(assemblyName))
+                    return Assembly.LoadFrom(assemblyPath);
+                return null;
+            };
+        }
+
+        private static readonly object _lockObj = new object();
+
+        public IntPtr GetFunctor(string assemblyPath, string typeName, string function)
         {
             if (_disposed)
                 throw new InvalidOperationException("Domain is already disposed");
 
-            var key = (assemblyPath, typeName, function);
-
-            EntryPoint result;
-
-            if (!_delegates.TryGetValue(key, out result))
+            // neither the domain data nor the _functors dictionary is threadsafe
+            lock (_lockObj)
             {
-                var assembly = Domain.Load(AssemblyName.GetAssemblyName(assemblyPath));
-                var type = assembly.GetType(typeName, throwOnError: true);
+                installResolver(assemblyPath);
+                var assemblyName = AssemblyName.GetAssemblyName(assemblyPath).Name;
 
-                Print($"Loaded type {type}");
-                result = (EntryPoint)Delegate.CreateDelegate(typeof(EntryPoint), type, function);
+                var key = (assemblyName, typeName, function);
 
-                _delegates[key] = result;
+                IntPtr result;
+                if (!_functors.TryGetValue(key, out result))
+                {
+                    Domain.SetData("_assemblyPath", assemblyPath);
+                    Domain.SetData("_typeName", typeName);
+                    Domain.SetData("_function", function);
+
+                    Domain.DoCallBack(new CrossAppDomainDelegate(DomainSetup.StoreFunctorFromDomainData));
+                    result = (IntPtr)Domain.GetData("_thisFunctor");
+                    if (result == IntPtr.Zero)
+                        throw new Exception($"Unable to get functor for {assemblyName}, {typeName}, {function}");
+
+                    // set inputs to StoreFunctorFromDomainData to null.
+                    // (There's no method to explicitly clear domain data)
+                    Domain.SetData("_assemblyPath", null);
+                    Domain.SetData("_typeName", null);
+                    Domain.SetData("_function", null);
+
+                    // the result has to remain in the domain data because we don't know when the
+                    // client of pyclr_get_function will actually invoke the functor, and if we
+                    // remove it from the domain data after returning it may get collected too early.
+                    _functors[key] = result;
+                }
+                return result;
             }
-
-            return result;
         }
 
         public void Dispose()
         {
             if (!_disposed)
             {
-                _delegates.Clear();
+                _functors.Clear();
 
                 if (Domain != AppDomain.CurrentDomain)
                     AppDomain.Unload(Domain);

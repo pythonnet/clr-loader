055a44b01acb08181e61ea372b603c9a459cb011
Metadorius <crabiter@vivaldi.net>
2026-03-05 20:36:10 +0200

    Target net461

--- netfx_loader/ClrLoader.cs
+++ netfx_loader/ClrLoader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Text;
 using NXPorts.Attributes;
 
 namespace ClrLoader
@@ -11,6 +12,21 @@ namespace ClrLoader
     {
         static bool _initialized = false;
         static List<DomainData> _domains = new List<DomainData>();
+
+        private static string PtrToStringUtf8(IntPtr ptr)
+        {
+            if (ptr == IntPtr.Zero)
+                return null;
+
+            int len = 0;
+            while (Marshal.ReadByte(ptr, len) != 0)
+                len++;
+
+            byte[] bytes = new byte[len];
+            Marshal.Copy(ptr, bytes, 0, len);
+
+            return Encoding.UTF8.GetString(bytes);
+        }
 
         [DllExport("pyclr_initialize", CallingConvention.Cdecl)]
         public static void Initialize()
@@ -36,11 +52,10 @@ namespace ClrLoader
         }
 
         [DllExport("pyclr_create_appdomain", CallingConvention.Cdecl)]
-        public static IntPtr CreateAppDomain(
-            [MarshalAs(UnmanagedType.LPUTF8Str)] string name,
-            [MarshalAs(UnmanagedType.LPUTF8Str)] string configFile
-        )
+        public static IntPtr CreateAppDomain(IntPtr namePtr, IntPtr configFilePtr)
         {
+            string name = PtrToStringUtf8(namePtr);
+            string configFile = PtrToStringUtf8(configFilePtr);
             Print($"Creating AppDomain {name} with {configFile}");
 
             var clrLoaderDir = AssemblyDirectory;
@@ -68,13 +83,16 @@ namespace ClrLoader
         [DllExport("pyclr_get_function", CallingConvention.Cdecl)]
         public static IntPtr GetFunction(
             IntPtr domain,
-            [MarshalAs(UnmanagedType.LPUTF8Str)] string assemblyPath,
-            [MarshalAs(UnmanagedType.LPUTF8Str)] string typeName,
-            [MarshalAs(UnmanagedType.LPUTF8Str)] string function
+            IntPtr assemblyPathPtr,
+            IntPtr typeNamePtr,
+            IntPtr functionPtr
         )
         {
             try
             {
+                string assemblyPath = PtrToStringUtf8(assemblyPathPtr);
+                string typeName = PtrToStringUtf8(typeNamePtr);
+                string function = PtrToStringUtf8(functionPtr);
                 var domainData = _domains[(int)domain];
                 Print($"Getting functor for function {function} of type {typeName} in assembly {assemblyPath}");
                 return domainData.GetFunctor(assemblyPath, typeName, function);
